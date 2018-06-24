1186d3ca87c58bff79d31674b1d50acbc0d92c15
Anton Bohomol <anton.bogomol@gmail.com>
2018-06-24 22:49:52 +0200

    Update sample app

--- sample/src/main/java/com/abohomol/sample/MainActivity.kt
+++ sample/src/main/java/com/abohomol/sample/MainActivity.kt
@@ -3,25 +3,27 @@ package com.abohomol.sample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.abohomol.kukot.KuCoin
-import kotlinx.android.synthetic.main.activity_main.helloWorld
+import kotlinx.android.synthetic.main.activity_main.button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        helloWorld.setOnClickListener { start() }
+        button.setOnClickListener { showUserEmail() }
     }
 
-    private fun start() {
+    private fun showUserEmail() {
         val service = KuCoin.create(API_KEY, SECRET)
         service.getUserProfile()
                 .subscribe({
-                               Log.e("KUCOIN", "Success $it")
-                           }, {
-                               Log.e("KUCOIN", "Failure", it)
-                           })
+                    Toast.makeText(this, "User email: ${it.email}", Toast.LENGTH_LONG).show()
+                }, {
+                    Toast.makeText(this, "Request failed", Toast.LENGTH_LONG).show()
+                    Log.e("KUCOIN", "Failure", it)
+                })
     }
 
     companion object {
